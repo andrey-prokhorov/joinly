1ad6469fc38dd7785e8e4ae229bddd86bbc9f1d8
Andrey Prokhorov <andrey.prokhorov@forefront.se>
2026-02-25 20:37:20 +0100

    feat(app): show "login" and "register" buttons when not logged in

--- app/src/pages/StartPage/StartPage.tsx
+++ app/src/pages/StartPage/StartPage.tsx
@@ -1,44 +1,42 @@
-import { Stack, Typography, Box } from "@mui/material"
+import { Button, Stack, Typography } from "@mui/material"
 import { Link } from "@tanstack/react-router"
+import { useAuth } from "@/useAuth"
 import { InfoBox } from "../../components/InfoBox/InfoBox"
 import { PageLayout } from "../../components/PageLayout/PageLayout"
 
-export const StartPage = () => (
-	<PageLayout>
-		<Typography variant="h4" sx={{ fontWeight: 700, mb: 7 }}>
-			Startsida
-		</Typography>
+export const StartPage = () => {
+	const { isAuthenticated } = useAuth()
 
-		<InfoBox>
-			<Stack spacing={3} textAlign="left" py={2}>
-				<Typography variant="h6">
-					Välkommen till <span color="accent.primary">joinly</span>!
-				</Typography>
+	return (
+		<PageLayout>
+			<Typography variant="h4" sx={{ fontWeight: 700, mb: 7 }}>
+				Startsida
+			</Typography>
 
-                <Typography variant="body1">
-                    Joinly är en app för att hitta sällskap för träning – snabbt och utan krångel. Här kan du enkelt hitta eller
-                    skapa aktiviteter som löpning, cykling eller motorcykelturer i närheten av dig. Du behöver inte gå med i
-                    grupper eller planera långt i förväg – se vad som händer idag eller imorgon och häng på.
-                </Typography>
-            </Stack>
-        </InfoBox>
-        <Typography variant="body2">
-            Logga in eller registrera dig för att komma igång!{" "}
-            <Box
-                component={Link}
-                to="/login"
-                sx={{
-                    color: "primary.main",
-                    textDecoration: "none",
-                    fontWeight: 600,
-                    display: "inline",
-                    "&:hover": {
-                        textDecoration: "underline",
-                    },
-                }}
-            >
-                Logga in här
-            </Box>
-        </Typography>
-    </PageLayout>
-)
+			<InfoBox>
+				<Stack spacing={3} textAlign="left" py={2}>
+					<Typography variant="h6">
+						Välkommen till <span color="accent.primary">joinly</span>!
+					</Typography>
+
+					<Typography variant="body1">
+						Joinly är en app för att hitta sällskap för träning – snabbt och utan krångel. Här kan du enkelt hitta eller
+						skapa aktiviteter som löpning, cykling eller motorcykelturer i närheten av dig. Du behöver inte gå med i
+						grupper eller planera långt i förväg – se vad som händer idag eller imorgon och häng på.
+					</Typography>
+				</Stack>
+			</InfoBox>
+
+			{!isAuthenticated && (
+				<Stack direction="row" spacing={2} sx={{ justifyContent: "center", alignItems: "center" }}>
+					<Button variant="contained" color="primary" component={Link} to="/login" sx={{ mb: 2 }}>
+						Logga in
+					</Button>
+					<Button variant="contained" color="primary" component={Link} to="/register" sx={{ mb: 2 }}>
+						Registera dig
+					</Button>
+				</Stack>
+			)}
+		</PageLayout>
+	)
+}
