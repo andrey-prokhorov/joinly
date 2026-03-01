a92cb4dbf35b7c07523d21c4e37d302b444142c1
Andrey Prokhorov <andrey.prokhorov@forefront.se>
2026-03-01 23:31:15 +0100

    Added first tests in playwright

--- app/playwright/start.spec.ts
+++ app/playwright/start.spec.ts
@@ -1,11 +1,14 @@
 import { expect, test } from "@playwright/test";
 import { login } from "./utils/login";
 
-test.describe("Startsida: Admin", () => {
+test.describe("Startsida för roll: Admin", () => {
 	test("Startsida", async ({ page }) => {
 		await login(page);
+
+		await page.goto("/start");
+
 		await expect(
-			page.getByRole("heading", { name: "List med aktiviteter" }),
+			page.getByRole("heading", { name: "Välkommen till joinly" }),
 		).toBeVisible();
 	});
 });
--- app/playwright/utils/login.ts
+++ app/playwright/utils/login.ts
@@ -1,17 +1,16 @@
-import { expect, type Page } from "@playwright/test"
-
+import type { Page } from "@playwright/test";
 
 export const login = async (page: Page) => {
-    const loginUrl = "/login"
-    const username = process.env.E2E_TEST_USERNAME as string
-	const password = process.env.E2E_TEST_PASSWORD as string
+	const loginUrl = "/login";
+	const username = process.env.E2E_TEST_USERNAME as string;
+	const password = process.env.E2E_TEST_PASSWORD as string;
 
-	await page.goto(loginUrl)
-    await page.getByLabel('Epost').fill(username);
-    await page.getByLabel('Lösenord').fill(password);
+	await page.goto(loginUrl);
+	await page.getByLabel("Epost").fill(username);
+	await page.getByLabel("Lösenord").fill(password);
 
 	await Promise.all([
-		await page.getByRole('button', { name: 'Logga in' }).click(),
+		await page.getByRole("button", { name: "Logga in" }).click(),
 		page.waitForLoadState("networkidle").catch(() => {}),
-	])
-}
+	]);
+};
