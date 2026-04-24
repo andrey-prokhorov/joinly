e5fd849d4cead8f8848082891ab513c7dd5520a1
Andrey Prokhorov <andrey.prokhorov@forefront.se>
2026-04-24 11:34:14 +0200

    Updated path to db for docker container

--- api/src/db/database.ts
+++ api/src/db/database.ts
@@ -14,7 +14,8 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // databasfilen:
-const dbPath = path.join(__dirname, "../../data/joinly.db")
+const dbPath =
+	process.env.DB_PATH || path.join(__dirname, "../../data/joinly.db");
 
 // Skapa data-mappen om den inte finns (behövs i CI och vid första körning)
 const dataDir = path.dirname(dbPath)
